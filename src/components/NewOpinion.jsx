beab0f1ee63b050088bb6530df02df331f8da8c1
VitorRamires <vmr.rs@hotmail.com>
2026-04-15 11:04:17 -0300

    adding opinion

--- src/components/NewOpinion.jsx
+++ src/components/NewOpinion.jsx
@@ -1,34 +1,37 @@
-import { useActionState } from "react";
-
-function handleSubmitOpinion(prevFormState, formData) {
-  const userName = formData.get("userName");
-  const title = formData.get("title");
-  const body = formData.get("body");
-
-  const errors = [];
-
-  if (userName === "") {
-    errors.push("fill the name input");
-  }
-
-  if (title === "") {
-    errors.push("fill the title input");
-  }
-
-  if (body === "") {
-    errors.push("fill the your opinion text area");
-  }
-
-  if (errors.length > 0) return { errors, inputValues: { userName, title, body }};
-  return { errors: null };
-}
-
-
+import { useActionState, use } from "react";
+import { OpinionsContext } from "../store/opinions-context";
 
 export function NewOpinion() {
+  const { addOpinion } = use(OpinionsContext);
   const [formState, formAction] = useActionState(handleSubmitOpinion, {
     errors: null,
   });
+
+  async function handleSubmitOpinion(prevFormState, formData) {
+    const userName = formData.get("userName");
+    const title = formData.get("title");
+    const body = formData.get("body");
+
+    const errors = [];
+
+    if (userName === "") {
+      errors.push("fill the name input");
+    }
+
+    if (title === "") {
+      errors.push("fill the title input");
+    }
+
+    if (body === "") {
+      errors.push("fill the your opinion text area");
+    }
+
+    if (errors.length > 0)
+      return { errors, inputValues: { userName, title, body } };
+
+    await addOpinion({ title, body, userName });
+    return { errors: null };
+  }
 
   return (
     <div id="new-opinion">
@@ -37,17 +40,32 @@ export function NewOpinion() {
         <div className="control-row">
           <p className="control">
             <label htmlFor="userName">Your Name</label>
-            <input type="text" id="userName" name="userName" defaultValue={formState.inputValues?.userName} />
+            <input
+              type="text"
+              id="userName"
+              name="userName"
+              defaultValue={formState.inputValues?.userName}
+            />
           </p>
 
           <p className="control">
             <label htmlFor="title">Title</label>
-            <input type="text" id="title" name="title" defaultValue={formState.inputValues?.title} />
+            <input
+              type="text"
+              id="title"
+              name="title"
+              defaultValue={formState.inputValues?.title}
+            />
           </p>
         </div>
         <p className="control">
           <label htmlFor="body">Your Opinion</label>
-          <textarea id="body" name="body" rows={5} defaultValue={formState.inputValues?.body}></textarea>
+          <textarea
+            id="body"
+            name="body"
+            rows={5}
+            defaultValue={formState.inputValues?.body}
+          ></textarea>
         </p>
 
         <p className="actions">
